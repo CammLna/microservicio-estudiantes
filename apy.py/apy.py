cc293bebc111f14715e005c73058c196fcb58916
Camila <cam.luna@duocuc.cl>
2026-09-04 22:39:00 -0400

    ci: add github actions workflow for develop branch

---
+++ apy.py/apy.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({
+        "status": "online",
+        "service": "Microservicio DevOps",
+        "version": "1.0.0"
+    }), 200
+
+@app.route("/health", methods=["GET"])
+def health():
+    return jsonify({"status": "healthy"}), 200
+
+# --- NUEVA FUNCIONALIDAD (FEATURE) ---
+@app.route("/api/v1/info", methods=["GET"])
+def info():
+    return jsonify({
+        "proyecto": "Evaluacion Parcial 1 DevOps",
+        "integrantes": 3,
+        "estado": "en desarrollo"
+    }), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
